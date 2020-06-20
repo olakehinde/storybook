c77c5a7ee775e5efcf84f8efcd057397e20f9528
olakehinde <olakehinde59@gmail.com>
2020-06-20 16:18:21 +0100

    added morgan for logging

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require("express")
+const morgan = require('morgan')
 const dotenv = require('dotenv')
+const exphbs = require('express-handlebars')
+const connectDB = require('./config/db')
 
 // load env config
 dotenv.config({ path: './config/config.env'})
 
+// test mongoDB connection
+connectDB()
+
 const app = express()
 
-const PORT = process.env.PORT || 3000
 
-// port to listen to 
+// Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+// Handlebars
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', '.hbs')
+
+// port to listen to
+const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
