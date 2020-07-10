d466234d0a3b208b57812916218ba66f471c558f
olakehinde <olakehinde59@gmail.com>
2020-07-10 14:14:35 +0100

    Added GET route for showing edit page

--- routes/stories.js
+++ routes/stories.js
@@ -23,7 +23,7 @@ router.post('/', ensureAuth, async (req, res) => {
     }
 })
 
-// @desc    show all sstories
+// @desc    show all stories
 // @route   GET /stories
 router.get('/', ensureAuth, async (req, res) => {
     try {
@@ -36,6 +36,26 @@ router.get('/', ensureAuth, async (req, res) => {
     } catch (err) {
         console.error(err)
         res.render('error/500')
+    }
+})
+
+// @desc    show edit page
+// @route   GET /stories/edit/:id
+router.get('/edit/:id', ensureAuth, async (req, res) => {
+    const story = await Story.findOne({
+        _id: req.params.id,
+    }).lean()
+
+    if (!story) {
+        return res.render('error/404')
+    }
+
+    if (story.user != req.user.id) {
+        res.redirect('/stories')
+    } else {
+        res.render('stories/edit', {
+            story,
+        })
     }
 })
 
